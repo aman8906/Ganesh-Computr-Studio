c46e3731c6e3e8255e4e6e125cc7d9d99224c2a3
Aman Chaursiya <ftpchaursiyaaman@gmail.com>
2026-09-25 14:49:30 +0530

    ENV

--- server/app.js
+++ server/app.js
@@ -17,14 +17,22 @@ export function createApp() {
   const app = express();
 
   app.use(helmet());
-
+  const allowedOrigins = [
+  'http://localhost:5173',
+  'https://ganesh-com-std.netlify.app',
+];
   app.use(
-    cors({
-      origin: process.env.CLIENT_URL || 'http://localhost:5173',
-      credentials: true,
-    })
-  );
-
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
   app.use(express.json({ limit: '100kb' }));
   app.use(cookieParser());
 
